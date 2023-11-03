2be203d11e0a099de35416a4e8d15ba1ae93e437
Mega Mind <Mega-Me@users.noreply.github.com>
2023-11-03 20:31:00 +0300

    initial comit

---
+++ function.php
@@ -0,0 +1,19 @@
+<?php
+function check_login($conn){
+ if(isset($_SESSION['email']))
+ {
+    $email =$_SESSION['email'];
+    $query ="select * from signup where email='$email' limit 1";
+
+    $result = mysqli_query($conn,$query);
+    if($result && mysqli_num_rows($result)>0)
+    {
+        $user_data = mysqli_fetch_assoc($result);
+        return $user_data;
+    }
+ }
+
+ //redirect to login
+ header("location:login.php");
+ die;
+}
